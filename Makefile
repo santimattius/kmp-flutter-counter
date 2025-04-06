0c3fd548fe6c72e6823acf4e11f374cae1b05b95
Santiago Mattiauda <santimattius@gmail.com>
2025-04-06 17:20:38 -0300

    [feature] setup amper CLI

---
+++ Makefile
@@ -0,0 +1,5 @@
+android:
+	./amper run --module android-app
+
+ios:
+	./amper run --module ios-app --platform iosSimulatorArm64
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,9 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
+    id("org.jetbrains.amper.settings.plugin").version("0.6.0")
 }
+
+include(":android-app")
+include(":ios-app")
+include(":shared")
